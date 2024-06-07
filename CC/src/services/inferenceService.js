69b2deb6da35d4522f1598fb8fab2dd119380f39
Noitusanx <habilnasutionx@gmail.com>
2024-06-07 21:44:34 +0700

    feat: store data discussions and comment in firestore

--- CC/src/services/inferenceService.js
+++ CC/src/services/inferenceService.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // prepocessing image, predict image, and return the result
 
 const tf = require('@tensorflow/tfjs-node');
@@ -40,32 +39,3 @@ async function predictClassification(model, imageBuffer) {
 
 
 module.exports = predictClassification;
-=======
-// prepocessing image, predict image, and return the result
-
-const tf = require('@tensorflow/tfjs-node');
-const InputError = require('../exceptions/InputError');
-
-async function predictClassification(model, imageBuffer) {
-  try {
-    const tensor = tf.node
-      .decodeJpeg(imageBuffer)
-      .resizeNearestNeighbor([224, 224])
-      .expandDims()
-      .toFloat();
-
-    const prediction = model.predict(tensor);
-    const score = await prediction.data();
-    const resultScore = Math.max(...score) * 100;
-    const result = resultScore > 50 ? 'Cancer' : 'Non-cancer';
-
-    const suggestion = result === 'Cancer' ? 'Segera periksa ke dokter!' : 'Anda sehat!';
-
-    return { resultScore, result, suggestion };
-  } catch (error) {
-    throw new InputError('Terjadi kesalahan dalam melakukan prediksi');
-  }
-}
-
-module.exports = predictClassification;
->>>>>>> 6ca4c64f9c7e943f7cc8bee7a21203745d31c941
