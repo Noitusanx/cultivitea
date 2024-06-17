dc4507e7d7964eb500e463b905296bef1983224e
NEBULAHIMB <ibrahimbintang0208@gmail.com>
2024-06-17 21:36:49 +0700

    sort by earliset

--- CC/src/server/handlers/discussionHandler.js
+++ CC/src/server/handlers/discussionHandler.js
@@ -1,176 +1,173 @@
-const {saveDiscussion, updateDiscussion, getAllDiscussions, getDiscussionById, deleteDiscussion} = require('../../services/storeData');
+const { saveDiscussion, updateDiscussion, getAllDiscussions, getDiscussionById, deleteDiscussion } = require('../../services/storeData');
 const crypto = require('crypto');
 
 // Discussion handler
 async function createDiscussion(req, res, next) {
-    const { title, content } = req.body;
-  
-    const discussionId = crypto.randomUUID();
-    const createdAt = new Date().toISOString();
-  
-    if (!req.user || !req.user.uid) {
-      console.error('User not authenticated');
-      return res.status(400).json({ 
-        error: true, 
-        message: 'User not authenticated' 
-      });
-    }
-  
-    const creator = req.user.username;
-    const creatorUid = req.user.uid;
+  const { title, content } = req.body;
 
-    if (!title || !content) {
-      return res.status(400).json({ 
-        error: true, 
-        message: 'Title and content are required to create a discussion' 
-      });
-  }
+  const discussionId = crypto.randomUUID();
+  const createdAt = new Date().toISOString();
 
-   
-  
-    const newDiscussion = {
-      discussionId,
-      creatorUid,
-      creator,
-      title,
-      content,
-      createdAt,
-    };
-  
-    await saveDiscussion(newDiscussion);
-  
-    res.status(201).json({ 
-      error: false, 
-      message: 'Discussion created successfully', 
-      data: newDiscussion 
+  if (!req.user || !req.user.uid) {
+    console.error('User not authenticated');
+    return res.status(400).json({
+      error: true,
+      message: 'User not authenticated'
     });
   }
-  
-  
-  async function getAllDiscussionsHandler(req, res, next) {
-    try {
-      const discussion = await getAllDiscussions()
-      res.status(200).json({ 
-        error: false, 
-        message: 'All discussions retrieved successfully', 
-        data: discussion
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
-  
-  
-  async function getDiscussionHandler(req, res, next) {
-    const { discussionId } = req.params;
-  
-    try {
-      const discussion = await getDiscussionById(discussionId);
-  
-  
-      if (!discussion) {
-        return res.status(404).json({ 
-          error: true, 
-          message: 'Discussion not found' 
-        });
-      }
-  
-      res.status(200).json({ 
-        error: false, 
-        message: 'Discussion retrieved successfully', 
-        data: discussion 
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
-  
-  async function updateDiscussionHandler(req, res, next) {
-    const { discussionId } = req.params;
-    const { title, content } = req.body;
-  
-    if (!req.user || !req.user.uid) {
-      return res.status(400).json({ 
-        error: true, 
-        message: 'User not authenticated' 
-      });
-    }
-  
-    const uid = req.user.uid;
-    const discussion = await getDiscussionById(discussionId);
-  
-    if (!discussion) {
-      console.error('Discussion not found');
-      return res.status(404).json({ 
-        error: true, 
-        message: 'Discussion not found' 
-      });
-    }
-  
-    if (discussion.creatorUid !== uid) {
-      console.error('User not authorized');
-      return res.status(403).json({ 
-        error: true, 
-        message: 'You are not authorized to update this discussion'
-      });
-    }
-  
-    const updatedData = {
-      title,
-      content,
-    };
-  
-    try {
-      await updateDiscussion(discussionId, updatedData);
-      res.status(200).json({ 
-        error: false, 
-        message: 'Discussion updated successfully' 
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
-  
-  
-  async function deleteDiscussionHandler(req, res, next) {
-    const { discussionId } = req.params;
-  
-    if (!req.user || !req.user.uid) {
-      console.error('User not authenticated');
-      return res.status(400).json({ 
-        error: true, 
-        message: 'User not authenticated' 
-      });
-    }
-  
-    const uid = req.user.uid;
-    const discussion = await getDiscussionById(discussionId);   
-  
-    if (!discussion) {
-      console.error('Discussion not found');
-      return res.status(404).json({ 
-        error: true, 
-        message: 'Discussion not found' 
-      });
-    }
-  
-    if (discussion.creatorUid !== uid) {
-      console.error('User not authorized');
-      return res.status(403).json({ 
-        error: true, 
-        message: 'You are not authorized to delete this discussion'
-      });
-    }
-  
-    try {
-      await deleteDiscussion(discussionId);
-      res.status(200).json({ 
-        error: false, 
-        message: 'Discussion deleted successfully' 
-      });
-    } catch (error) {
-      next(error);
-    }
+
+  const creator = req.user.username;
+  const creatorUid = req.user.uid;
+
+  if (!title || !content) {
+    return res.status(400).json({
+      error: true,
+      message: 'Title and content are required to create a discussion'
+    });
   }
 
-  module.exports = {createDiscussion, getAllDiscussionsHandler, getDiscussionHandler, updateDiscussionHandler, deleteDiscussionHandler};
+  const newDiscussion = {
+    discussionId,
+    creatorUid,
+    creator,
+    title,
+    content,
+    createdAt,
+  };
+
+  await saveDiscussion(newDiscussion);
+
+  res.status(201).json({
+    error: false,
+    message: 'Discussion created successfully',
+    data: newDiscussion
+  });
+}
+
+async function getAllDiscussionsHandler(req, res, next) {
+  try {
+    let discussions = await getAllDiscussions();
+    // Sort discussions by createdAt in ascending order (earliest first)
+    discussions.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+
+    res.status(200).json({
+      error: false,
+      message: 'All discussions retrieved successfully',
+      data: discussions
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getDiscussionHandler(req, res, next) {
+  const { discussionId } = req.params;
+
+  try {
+    const discussion = await getDiscussionById(discussionId);
+
+    if (!discussion) {
+      return res.status(404).json({
+        error: true,
+        message: 'Discussion not found'
+      });
+    }
+
+    res.status(200).json({
+      error: false,
+      message: 'Discussion retrieved successfully',
+      data: discussion
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateDiscussionHandler(req, res, next) {
+  const { discussionId } = req.params;
+  const { title, content } = req.body;
+
+  if (!req.user || !req.user.uid) {
+    return res.status(400).json({
+      error: true,
+      message: 'User not authenticated'
+    });
+  }
+
+  const uid = req.user.uid;
+  const discussion = await getDiscussionById(discussionId);
+
+  if (!discussion) {
+    console.error('Discussion not found');
+    return res.status(404).json({
+      error: true,
+      message: 'Discussion not found'
+    });
+  }
+
+  if (discussion.creatorUid !== uid) {
+    console.error('User not authorized');
+    return res.status(403).json({
+      error: true,
+      message: 'You are not authorized to update this discussion'
+    });
+  }
+
+  const updatedData = {
+    title,
+    content,
+  };
+
+  try {
+    await updateDiscussion(discussionId, updatedData);
+    res.status(200).json({
+      error: false,
+      message: 'Discussion updated successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteDiscussionHandler(req, res, next) {
+  const { discussionId } = req.params;
+
+  if (!req.user || !req.user.uid) {
+    console.error('User not authenticated');
+    return res.status(400).json({
+      error: true,
+      message: 'User not authenticated'
+    });
+  }
+
+  const uid = req.user.uid;
+  const discussion = await getDiscussionById(discussionId);
+
+  if (!discussion) {
+    console.error('Discussion not found');
+    return res.status(404).json({
+      error: true,
+      message: 'Discussion not found'
+    });
+  }
+
+  if (discussion.creatorUid !== uid) {
+    console.error('User not authorized');
+    return res.status(403).json({
+      error: true,
+      message: 'You are not authorized to delete this discussion'
+    });
+  }
+
+  try {
+    await deleteDiscussion(discussionId);
+    res.status(200).json({
+      error: false,
+      message: 'Discussion deleted successfully'
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { createDiscussion, getAllDiscussionsHandler, getDiscussionHandler, updateDiscussionHandler, deleteDiscussionHandler };
--- CC/src/server/handlers/predictHandler.js
+++ CC/src/server/handlers/predictHandler.js
@@ -1,4 +1,4 @@
-const { storeData, getPredictHistories, uploadPredictionImageToStorage, } = require('../../services/storeData');
+const { storeData, getPredictHistories, uploadPredictionImageToStorage } = require('../../services/storeData');
 const InputError = require('../../exceptions/InputError');
 const loadModel = require('../../services/loadModel');
 const crypto = require('crypto');
@@ -6,67 +6,72 @@ const predictClassification = require('../../services/inferenceService');
 
 // new predict handler
 async function postPredict(req, res, next) {
-    try {
-      const uid = req.user.uid;
-      const { file: image } = req;
-      if (!image) {
-        throw new InputError('Invalid input: No image provided');
-      }
-  
-      const imageUrl = await uploadPredictionImageToStorage(image)
-  
-      const model = await loadModel();
-      const { result, suggestion } = await predictClassification(model, image.buffer);
-  
-      const teaPlantId = crypto.randomUUID();
-      const createdAt = new Date().toISOString();
-  
-      const predictionResult = {
-        teaPlantId,
-        imageUrl,
-        result,
-        suggestion,
-        createdAt,
-      };
-  
-      const storeResult = await storeData(uid, predictionResult);
-  
-      if (!storeResult.success) {
-        throw new Error(storeResult.error);
-      }
-  
-      res.status(201).json({
-        error: false,
-        message: 'Model is predicted successfully',
-        data: predictionResult,
-      });
-    } catch (error) {
-      if (error instanceof InputError) {
-        res.status(400).json({ 
-          error: true, 
-          message: error.message 
-        });
-      } else {
-        next(error);
-      }
+  try {
+    const uid = req.user.uid;
+    const { file: image } = req;
+    if (!image) {
+      throw new InputError('Invalid input: No image provided');
     }
-  }
-  
-  async function getPredictHistoriesHandler(req, res, next) { 
-    try {
-      const uid = req.user.uid;
-      const histories = await getPredictHistories(uid);
-      const predictionHistories = histories.map(item => ({
-        id: item.teaPlantId,
-        history: item,
-      }));
-      res.status(200).json({ 
-        error: false, 
-        message: "Predictions history retrieved successfully", 
-        data: predictionHistories });
-    } catch (error) {
+
+    const imageUrl = await uploadPredictionImageToStorage(image);
+
+    const model = await loadModel();
+    const { result, suggestion } = await predictClassification(model, image.buffer);
+
+    const teaPlantId = crypto.randomUUID();
+    const createdAt = new Date().toISOString();
+
+    const predictionResult = {
+      teaPlantId,
+      imageUrl,
+      result,
+      suggestion,
+      createdAt,
+    };
+
+    const storeResult = await storeData(uid, predictionResult);
+
+    if (!storeResult.success) {
+      throw new Error(storeResult.error);
+    }
+
+    res.status(201).json({
+      error: false,
+      message: 'Model is predicted successfully',
+      data: predictionResult,
+    });
+  } catch (error) {
+    if (error instanceof InputError) {
+      res.status(400).json({ 
+        error: true, 
+        message: error.message 
+      });
+    } else {
       next(error);
     }
   }
+}
 
-  module.exports = {postPredict, getPredictHistories, getPredictHistoriesHandler};
+async function getPredictHistoriesHandler(req, res, next) { 
+  try {
+    const uid = req.user.uid;
+    const histories = await getPredictHistories(uid);
+
+    // Sort histories by createdAt in ascending order (earliest first)
+    histories.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+
+    const predictionHistories = histories.map(item => ({
+      history: item,
+    }));
+    
+    res.status(200).json({ 
+      error: false, 
+      message: "Predictions history retrieved successfully", 
+      data: predictionHistories 
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+module.exports = { postPredict, getPredictHistories, getPredictHistoriesHandler };
