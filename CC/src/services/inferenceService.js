b4f867e681c7694669f71ee87ee1fea1e7ee82d5
Noitusanx <habilnasutionx@gmail.com>
2024-06-18 18:33:00 +0700

    feat: backup if firebase auth doesn't work

--- CC/src/services/inferenceService.js
+++ CC/src/services/inferenceService.js
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-// prepocessing image, predict image, and return the result
-
-const tf = require('@tensorflow/tfjs-node');
-const InputError = require('../exceptions/InputError');
-
-
-async function predictClassification(model, imageBuffer) {
-  try {
-    const tensor = tf.node
-      .decodeJpeg(imageBuffer)
-      .resizeNearestNeighbor([224, 224])
-      .toFloat()
-      .expandDims();
-
-    const prediction = model.predict(tensor);
-    const score = await prediction.data();
-    
-    const labels = ['Tea Algal Leaf', 'Tea Anthracnose', 'Tea Bird Eye Spot', 'Tea Brown Blight', 'Tea Healthy', 'Tea Red Leaf Spot']
-
-    const predictedIndex = score.indexOf(Math.max(...score));
-    const result = labels[predictedIndex];
-    const accuracy = Math.max(...score) * 100;
-
-    const suggestion = result !== 'Tea Healthy' ? 'Segera periksa tanaman Anda!' : 'Tanaman Anda sehat!';
-
-    return { result, suggestion, accuracy };
-  } catch (error) {
-    console.error("Error in predictClassification: ", error);
-    throw new InputError('Terjadi kesalahan dalam melakukan prediksi');
-  }
-}
-
-
-
-
-module.exports = predictClassification;
-=======
 // // prepocessing image, predict image, and return the result
 
 const tf = require('@tensorflow/tfjs-node');
@@ -72,4 +34,3 @@ module.exports = predictClassification;
 
 
 
->>>>>>> 3ec16a9 (feat: backup if firebase auth doesn't work)
--- CC/src/services/loadModel.js
+++ CC/src/services/loadModel.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Load the model from the URL provided in the .env file
 
 require('dotenv').config()
@@ -10,16 +9,3 @@ async function loadModel() {
 }
 
 module.exports = loadModel;
-=======
-// Load the model from the URL provided in the .env file
-
-require('dotenv').config()
-
-const tf = require('@tensorflow/tfjs-node');
-
-async function loadModel() {
-    return tf.loadLayersModel(process.env.MODEL_URL);
-}
-
-module.exports = loadModel;
->>>>>>> 3ec16a9 (feat: backup if firebase auth doesn't work)
