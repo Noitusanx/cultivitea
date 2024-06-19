461a480e2e86169d209af893fc0e76dc4b182f05
Noitusanx <habilnasutionx@gmail.com>
2024-06-19 20:15:21 +0700

    fix: suggestion result in the model

--- CC/src/services/inferenceService.js
+++ CC/src/services/inferenceService.js
@@ -18,12 +18,37 @@ async function predictClassification(model, imageBuffer) {
 
     const labels = ['Algal Spot', 'Brown Blight', 'Gray Blight', 'Healthy', 'Helopeltis', 'Red Spot'];
 
+
     const predictedIndex = score.indexOf(Math.max(...score));
     const result = labels[predictedIndex];
+    let suggestion;
 
-    const suggestion = result !== 'Healthy' ? 'Segera periksa tanaman Anda!' : 'Tanaman Anda sehat!';
+    switch (result) {
+      case 'Algal Spot':
+        suggestion = "Lakukan sanitasi lahan dengan membuang daun terinfeksi jauh dari lahan, pangkas daun yang terinfeksi, gunakan fungisida sesuai petunjuk, dan perbaiki sirkulasi udara dengan memangkas tanaman yang terlalu rapat.";
+        break;
+      case 'Brown Blight':
+        suggestion = "Kontrol kelembapan dengan memastikan tanah tidak terlalu lembab, pangkas daun terinfeksi, dan gunakan fungisida.";
+        break;
+      case 'Gray Blight':
+        suggestion = "Pangkas daun terinfeksi, gunakan fungisida, dan pastikan drainase lahan baik agar air tidak menggenang.";
+        break;
+      case 'Helopeltis':
+        suggestion = "Buang bagian tanaman yang terinfeksi, gunakan insektisida, dan perkenalkan predator alami seperti semut atau kepik dengan membawa mereka ke area lahan.";
+        break;
+      case 'Red Spot':
+        suggestion = "Buang daun terinfeksi, gunakan fungisida, dan pastikan kelembapan tanah terkontrol.";
+        break;
+      case 'Healthy':
+        suggestion = "Tanaman teh Anda dalam kondisi sehat!";
+        break;
+      default:
+        suggestion = "Segera periksa tanaman teh Anda!";
+    }
 
     return { result, suggestion };
+
+
   } catch (error) {
     console.error("Error in predictClassification: ", error);
     throw new InputError('Terjadi kesalahan dalam melakukan prediksi');
